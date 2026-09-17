0c85e7bb3de3bc5d4eb53cebc1835292978a5991
Student <Student@LabVM2049939.t4cotrdkls1unpf1qbtxltjvva.jnbx.internal.cloudapp.net>
2026-09-18 00:39:07 +0200

    Changed Username info for the placeholders under LoginTest.

--- src/test/java/com/mycompany/networkhub/LoginTest.java
+++ src/test/java/com/mycompany/networkhub/LoginTest.java
@@ -26,7 +26,7 @@ public class LoginTest {
 
     @Test
     public void testUsernameIncorrectlyFormatted() {
-        Login login = new Login("Kyle", "Smith", "kyle!!!!!!!",
+        Login login = new Login("Sbaluleko", "Mthembu", "kyle!!!!!!!",
                 "Ch&&sec@ke99!", "+27838968976");
         assertEquals("Username is not correctly formatted; please ensure that "
                 + "your username contains an underscore and is no more than "
@@ -35,7 +35,7 @@ public class LoginTest {
 
     @Test
     public void testPasswordMeetsComplexity() {
-        Login login = new Login("Kyle", "Smith", "kyl_1",
+        Login login = new Login("Sbaluleko", "Mthembu", "kyl_1",
                 "Ch&&sec@ke99!", "+27838968976");
         assertEquals("Password successfully captured.",
                 login.returnPasswordMessage());
@@ -43,7 +43,7 @@ public class LoginTest {
 
     @Test
     public void testPasswordDoesNotMeetComplexity() {
-        Login login = new Login("Kyle", "Smith", "kyl_1",
+        Login login = new Login("Sbaluleko", "Mthembu", "kyl_1",
                 "password", "+27838968976");
         assertEquals("Password is not correctly formatted; please ensure that "
                 + "the password contains at least eight characters, a capital "
@@ -53,7 +53,7 @@ public class LoginTest {
 
     @Test
     public void testCellPhoneCorrectlyFormatted() {
-        Login login = new Login("Kyle", "Smith", "kyl_1",
+        Login login = new Login("Sbaluleko", "Mthembu", "kyl_1",
                 "Ch&&sec@ke99!", "+27838968976");
         assertEquals("Cell number successfully captured.",
                 login.returnCellPhoneMessage());
@@ -61,7 +61,7 @@ public class LoginTest {
 
     @Test
     public void testCellPhoneIncorrectlyFormatted() {
-        Login login = new Login("Kyle", "Smith", "kyl_1",
+        Login login = new Login("Sbaluleko", "Mthembu", "kyl_1",
                 "Ch&&sec@ke99!", "08966553");
         assertEquals("Cell number is incorrectly formatted or does not contain "
                 + "an international code; please correct the number and try "
@@ -70,7 +70,7 @@ public class LoginTest {
 
     @Test
     public void testLoginSuccessfulMessage() {
-        Login login = new Login("Kyle", "Smith", "kyl_1",
+        Login login = new Login("Sbaluleko", "Mthembu", "kyl_1",
                 "Ch&&sec@ke99!", "+27838968976");
         assertEquals("Welcome Kyle, Smith it is great to see you again.",
                 login.returnLoginStatus(true));
@@ -78,7 +78,7 @@ public class LoginTest {
 
     @Test
     public void testLoginFailedMessage() {
-        Login login = new Login("Kyle", "Smith", "kyl_1",
+        Login login = new Login("Sbaluleko", "Mthembu", "kyl_1",
                 "Ch&&sec@ke99!", "+27838968976");
         assertEquals("Username or password incorrect, please try again.",
                 login.returnLoginStatus(false));
@@ -88,7 +88,7 @@ public class LoginTest {
 
     @Test
     public void testLoginSuccessful() {
-        Login login = new Login("Kyle", "Smith", "kyl_1",
+        Login login = new Login("Sbaluleko", "Mthembu", "kyl_1",
                 "Ch&&sec@ke99!", "+27838968976");
         assertTrue(login.loginUser("kyl_1", "Ch&&sec@ke99!"));
     }
